133c8165af3c78a2e556e7c371005aafa964b12e
Admin <baconnkl@gmail.com>
2022-03-04 18:00:11 +0300

    change import

--- app/src/main/java/com/example/rickandmortyarchitecture/base/BaseViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/base/BaseViewModel.kt
@@ -3,17 +3,16 @@ package com.example.rickandmortyarchitecture.base
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.rickandmortyarchitecture.common.resouce.Resource
+import com.bacon.common.resouce.Resource
 import com.example.rickandmortyarchitecture.presentation.state.UIState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
 
     protected fun <T> subscribeTo(
         state: MutableLiveData<UIState<T>>,
-        request: () -> Flow<Resource<T>>
+        request: () -> Flow<Resource<T>>,
     ) {
         viewModelScope.launch {
             request().collect {
