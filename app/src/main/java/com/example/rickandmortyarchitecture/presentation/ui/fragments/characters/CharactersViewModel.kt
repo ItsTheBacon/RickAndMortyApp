4b68d58032e0918195544e773959a6d27871c4a3
Admin <baconnkl@gmail.com>
2022-03-08 21:23:04 +0300

    --------------------------------------------

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -1,21 +1,22 @@
 package com.example.rickandmortyarchitecture.presentation.ui.fragments.characters
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.bacon.domain.models.CharactersModel
 import com.bacon.domain.usecase.CharacterUseCase
 import com.example.rickandmortyarchitecture.base.BaseFetch
 import com.example.rickandmortyarchitecture.base.BaseViewModel
+import com.example.rickandmortyarchitecture.presentation.models.CharactersUI
+import com.example.rickandmortyarchitecture.presentation.models.toUI
 import com.example.rickandmortyarchitecture.presentation.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
     private val useCase: CharacterUseCase,
 ) : BaseViewModel(), BaseFetch {
-    private val _charactersState = MutableLiveData<UIState<List<CharactersModel>>>()
-    val charactersState: LiveData<UIState<List<CharactersModel>>> = _charactersState
+    private val _charactersState = MutableStateFlow<UIState<List<CharactersUI>>>(UIState.Loading())
+    val charactersState: StateFlow<UIState<List<CharactersUI>>> = _charactersState
     override var page: Int = 1
 
     init {
@@ -23,8 +24,10 @@ class CharactersViewModel @Inject constructor(
     }
 
     override fun fetchRick(page: Int) {
-        subscribeTo(_charactersState) {
-            useCase(page)
+        useCase(page).collectRequest(_charactersState) { it ->
+          it.map {
+              it.toUI()
+          }
         }
     }
 }
