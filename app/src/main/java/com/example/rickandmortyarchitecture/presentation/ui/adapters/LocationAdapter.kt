54817d6bd93070bbdf80b760633f724519f16849
Admin <baconnkl@gmail.com>
2022-03-08 21:21:37 +0300

    minor changes in LocationAdapter.kt

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/adapters/LocationAdapter.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/adapters/LocationAdapter.kt
@@ -4,18 +4,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bacon.domain.models.LocationsModel
 import com.example.rickandmortyarchitecture.base.BaseDiffUtilCallback
 import com.example.rickandmortyarchitecture.databinding.ItemLocationRickBinding
+import com.example.rickandmortyarchitecture.presentation.models.LocationsUI
 
-class LocationAdapter : ListAdapter<LocationsModel, LocationAdapter.ViewHolder>(
+class LocationAdapter : ListAdapter<LocationsUI, LocationAdapter.ViewHolder>(
     BaseDiffUtilCallback()
 ) {
     inner class ViewHolder(
         private val binding: ItemLocationRickBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun onBind(data: LocationsModel) {
+        fun onBind(data: LocationsUI) {
             with(binding) {
                 name.text = data.name
                 description.text = data.created
@@ -25,19 +25,15 @@ class LocationAdapter : ListAdapter<LocationsModel, LocationAdapter.ViewHolder>(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(getItem(position))
+        getItem(position)?.let { holder.onBind(it) }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            ItemLocationRickBinding.inflate
-                (
+            ItemLocationRickBinding.inflate(
                 LayoutInflater.from(
                     parent.context
-                ),
-                parent,
-
-                false
+                ), parent, false
             )
         )
     }
