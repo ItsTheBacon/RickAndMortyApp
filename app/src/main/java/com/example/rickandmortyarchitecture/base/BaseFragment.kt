875a629a31eca9e52b361e0ba5466f341753119e
Admin <baconnkl@gmail.com>
2022-03-08 21:11:24 +0300

    added fun in BaseFragment.kt collectUIState

--- app/src/main/java/com/example/rickandmortyarchitecture/base/BaseFragment.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/base/BaseFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewbinding.ViewBinding
+import com.example.rickandmortyarchitecture.presentation.state.UIState
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 
-abstract class BaseFragment<V : BaseViewModel, B : ViewBinding>(
-    @LayoutRes layoutId: Int
+abstract class BaseFragment<V : BaseViewModel, BaseViewBinding : ViewBinding>(
+    @LayoutRes layoutId: Int,
 ) : Fragment(layoutId) {
 
-    protected abstract val binding: B
+    protected abstract val binding: BaseViewBinding
     protected abstract val viewModel: V
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,4 +47,16 @@ abstract class BaseFragment<V : BaseViewModel, B : ViewBinding>(
     open fun setupViews() {
     }
 
+    protected fun <T> StateFlow<UIState<T>>.collectUIState(
+        lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+        action: (UIState<T>) -> Unit,
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(lifecycleState) {
+                this@collectUIState.collect {
+                    action(it)
+                }
+            }
+        }
+    }
 }
