b6a40899a85f60342e76c3ea2077a0643626ee43
Admin <baconnkl@gmail.com>
2022-03-08 21:18:52 +0300

    added BaseBottomSheet.kt

---
+++ app/src/main/java/com/example/rickandmortyarchitecture/base/BaseBottomSheet.kt
@@ -0,0 +1,75 @@
+package com.example.rickandmortyarchitecture.base
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.viewbinding.ViewBinding
+import com.example.rickandmortyarchitecture.R
+import com.example.rickandmortyarchitecture.presentation.state.UIState
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseBottomSheet<Binding : ViewBinding, viewModel : BaseViewModel>(
+    @LayoutRes private val layoutId: Int,
+) : BottomSheetDialogFragment() {
+
+    protected abstract val binding: Binding
+    protected abstract val viewModel: viewModel
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        var dialog = super.onCreateDialog(savedInstanceState)
+        dialog = BottomSheetDialog(requireContext(), R.style.ThemeOverlay_App_BottomSheetDialog)
+        dialog.setOnShowListener { setupBottomSheetBackgroundTransparent(it) }
+        return dialog
+    }
+
+    private fun setupBottomSheetBackgroundTransparent(dialogInterface: DialogInterface) {
+        val bottomSheetDialog = dialogInterface as BottomSheetDialog
+        val bottomSheet = bottomSheetDialog.findViewById<View>(
+            com.google.android.material.R.id.design_bottom_sheet
+        ) ?: return
+        bottomSheet.setBackgroundColor(Color.TRANSPARENT)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+        setupListeners()
+    }
+
+    protected open fun initialize() {
+    }
+
+    protected open fun setupListeners() {
+    }
+
+    protected fun <T> StateFlow<UIState<T>>.collectUIState(
+        lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+        action: (UIState<T>) -> Unit,
+    ) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(lifecycleState) {
+                this@collectUIState.collect {
+                    action(it)
+                }
+            }
+        }
+    }
+}
+
