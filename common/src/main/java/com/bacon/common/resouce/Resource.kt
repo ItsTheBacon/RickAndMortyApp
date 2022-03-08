5165d88f5fc3e75dd063d52771657ca269c335a5
Admin <baconnkl@gmail.com>
2022-03-08 21:19:27 +0300

    Resource.kt

--- common/src/main/java/com/bacon/common/resouce/Resource.kt
+++ common/src/main/java/com/bacon/common/resouce/Resource.kt
@@ -2,12 +2,9 @@ package com.bacon.common.resouce
 
 sealed class Resource<out T>(
     val data: T? = null,
-    val message: String? = null
+    val message: String? = null,
 ) {
-
     class Loading<T>(data: T? = null) : Resource<T>(data = data)
     class Success<T>(data: T) : Resource<T>(data = data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data = data, message = message)
-
-
 }
