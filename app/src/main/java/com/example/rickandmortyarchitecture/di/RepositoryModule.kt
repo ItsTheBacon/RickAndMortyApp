ed713a823e0a3668974001190f662055eec61400
Admin <baconnkl@gmail.com>
2022-03-08 21:19:42 +0300

    provideCharacterDetailRepositoryImpl

--- app/src/main/java/com/example/rickandmortyarchitecture/di/RepositoryModule.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/di/RepositoryModule.kt
@@ -1,12 +1,15 @@
 package com.example.rickandmortyarchitecture.di
 
 import com.bacon.data.remote.apiservices.CharacterApiService
+import com.bacon.data.remote.apiservices.CharacterDetailApiService
 import com.bacon.data.remote.apiservices.EpisodesApiService
 import com.bacon.data.remote.apiservices.LocationApiService
 import com.bacon.data.repository.CharacterRepositoryImpl
+import com.bacon.data.repository.CharactersDetailRepositoryImpl
 import com.bacon.data.repository.EpisodesRepositoryImpl
 import com.bacon.data.repository.LocationRepositoryImpl
 import com.bacon.domain.repository.CharacterRepository
+import com.bacon.domain.repository.CharactersDetailRepository
 import com.bacon.domain.repository.EpisodesRepository
 import com.bacon.domain.repository.LocationRepository
 import dagger.Module
@@ -32,5 +35,10 @@ object RepositoryModule {
     @Provides
     fun provideEpisodesRepository(episodesApiService: EpisodesApiService): EpisodesRepository {
         return EpisodesRepositoryImpl(episodesApiService)
+    }
+
+    @Provides
+    fun provideCharactersDetailRepository(characterDetailApiService: CharacterDetailApiService): CharactersDetailRepository {
+        return CharactersDetailRepositoryImpl(characterDetailApiService)
     }
 }
