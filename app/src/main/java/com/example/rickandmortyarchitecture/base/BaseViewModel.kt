587076a634f7ea554f7f738c9b7e7375f23a47d1
Admin <baconnkl@gmail.com>
2022-03-08 21:11:40 +0300

    added fun in BaseViewModel.kt

--- app/src/main/java/com/example/rickandmortyarchitecture/base/BaseViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/base/BaseViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bacon.common.resouce.Resource
 import com.example.rickandmortyarchitecture.presentation.state.UIState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
@@ -29,6 +31,27 @@ abstract class BaseViewModel : ViewModel() {
                         it.data?.let { data ->
                             state.value = UIState.Success(data)
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    protected fun <T, S> Flow<Resource<T>>.collectRequest(
+        state: MutableStateFlow<UIState<S>>,
+        mappedData: (T) -> S
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            this@collectRequest.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        state.value = UIState.Loading()
+                    }
+                    is Resource.Error -> it.message?.let { error ->
+                        state.value = UIState.Error(error)
+                    }
+                    is Resource.Success -> it.data?.let { data ->
+                        state.value = UIState.Success(mappedData(data))
                     }
                 }
             }
