b46de7bd0c6715c4afec0772ed4d0f204fd8561e
Admin <baconnkl@gmail.com>
2022-03-08 21:12:38 +0300

    basic changes

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharacterFragment.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharacterFragment.kt
@@ -1,17 +1,16 @@
 package com.example.rickandmortyarchitecture.presentation.ui.fragments.characters
 
 import android.util.Log
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.bacon.domain.models.CharactersModel
 import com.example.rickandmortyarchitecture.R
 import com.example.rickandmortyarchitecture.base.BaseFragment
 import com.example.rickandmortyarchitecture.databinding.FragmentCharacterBinding
 import com.example.rickandmortyarchitecture.extensions.ScrollListener
+import com.example.rickandmortyarchitecture.extensions.isVisibleOrGone
+import com.example.rickandmortyarchitecture.extensions.navigateSafely
 import com.example.rickandmortyarchitecture.presentation.state.UIState
-import com.example.rickandmortyarchitecture.presentation.ui.activity.MainActivity
 import com.example.rickandmortyarchitecture.presentation.ui.adapters.CharactersAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,16 +19,15 @@ class CharacterFragment :
     BaseFragment<CharactersViewModel, FragmentCharacterBinding>(R.layout.fragment_character) {
     override val binding: FragmentCharacterBinding by viewBinding(FragmentCharacterBinding::bind)
     override val viewModel: CharactersViewModel by viewModels()
-    private val adapter = CharactersAdapter(this::onItemLongClick)
+    private val adapter = CharactersAdapter(
+        this::onItemLongClick,
+        this::setOnItemClickListener
+    )
+
 
     override fun initialize() {
         binding.characterRecycler.adapter = adapter
         binding.characterRecycler.ScrollListener(viewModel)
-
-    }
-
-    override fun setupListener() {
-        bottomNavigationItemReselectListener()
     }
 
     override fun setupObserve() {
@@ -37,32 +35,41 @@ class CharacterFragment :
     }
 
     private fun setUpCharacters() {
-        viewModel.charactersState.observe(viewLifecycleOwner) {
-            binding.progressBarEverything.isVisible = it is UIState.Loading
+        viewModel.charactersState.collectUIState {
             when (it) {
                 is UIState.Error -> {
                     Log.e("error", "Characters:${it.error} ")
                 }
                 is UIState.Loading -> {
+                    binding.shimmerLayout.startShimmer()
+
                 }
                 is UIState.Success -> {
-                    val list = ArrayList<CharactersModel>(adapter.currentList)
+                    binding.shimmerLayout.stopShimmer()
+                    binding.shimmerLayout.isVisibleOrGone(false)
+                    binding.characterRecycler.isVisibleOrGone(true)
+                    val list = ArrayList(adapter.currentList)
                     list.addAll(it.data)
                     adapter.submitList(list)
                 }
             }
         }
+
     }
 
     private fun onItemLongClick(photo: String) {
-        findNavController().navigate(
-            CharacterFragmentDirections.actionCharacterFragmentToDlalogFragment(photo)
+        findNavController().navigateSafely(
+            CharacterFragmentDirections
+                .actionCharacterFragmentToDlalogFragment
+                    (photo)
         )
     }
 
-    private fun bottomNavigationItemReselectListener() {
-        (requireActivity() as MainActivity).setOnBottomNavigationItemReselectListener {
-            binding.characterRecycler.smoothScrollToPosition(0)
-        }
+    private fun setOnItemClickListener(id: Int) {
+        findNavController().navigateSafely(
+            CharacterFragmentDirections
+                .actionCharacterFragmentToCharactersDetailFragment(id)
+        )
     }
+
 }
