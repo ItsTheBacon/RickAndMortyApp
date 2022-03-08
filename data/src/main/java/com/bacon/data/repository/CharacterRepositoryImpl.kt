8455f5db537962c6655c48fc3b80e94f1ce40f9a
Admin <baconnkl@gmail.com>
2022-03-08 21:12:49 +0300

    minor changes

--- data/src/main/java/com/bacon/data/repository/CharacterRepositoryImpl.kt
+++ data/src/main/java/com/bacon/data/repository/CharacterRepositoryImpl.kt
@@ -7,8 +7,9 @@ import com.bacon.data.repository.base.BaseRepository
 import com.bacon.domain.repository.CharacterRepository
 import javax.inject.Inject
 
-class CharacterRepositoryImpl @Inject constructor(private val service: CharacterApiService) :
-    BaseRepository(),
+class CharacterRepositoryImpl @Inject constructor(
+    private val service: CharacterApiService,
+) : BaseRepository(),
     CharacterRepository {
     override fun fetchCharacters(page: Int) = doRequest {
         service.fetchCharacters(page).toResponse().results.map {
