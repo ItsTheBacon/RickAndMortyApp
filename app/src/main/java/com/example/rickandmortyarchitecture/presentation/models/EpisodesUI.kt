332b00047ec3c74f0006295266c9428b449ea23a
Admin <baconnkl@gmail.com>
2022-03-11 15:47:27 +0300

    minor changes in UI EpisodesUI.kt

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/models/EpisodesUI.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/models/EpisodesUI.kt
@@ -1,15 +1,15 @@
 package com.example.rickandmortyarchitecture.presentation.models
 
 import com.bacon.domain.models.EpisodesModel
+import com.example.rickandmortyarchitecture.base.IBaseDiffModel
 
 data class EpisodesUI(
-
-    var id: Int? = null,
+    override val id: Int,
     var name: String? = null,
     var air_date: String? = null,
     var episode: String? = null,
     var created: String? = null,
-)
+) : IBaseDiffModel
 
 fun EpisodesModel.toUI() = EpisodesUI(
     id, name, air_date, episode, created
