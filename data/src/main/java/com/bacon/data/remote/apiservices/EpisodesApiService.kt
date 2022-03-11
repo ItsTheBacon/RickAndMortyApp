8427fb3ea5e5a17d7c7870c7c6fabe3424f8deee
Admin <baconnkl@gmail.com>
2022-03-11 16:52:17 +0300

    added request EpisodesApiService.kt details

--- data/src/main/java/com/bacon/data/remote/apiservices/EpisodesApiService.kt
+++ data/src/main/java/com/bacon/data/remote/apiservices/EpisodesApiService.kt
@@ -3,6 +3,7 @@ package com.bacon.data.remote.apiservices
 import com.bacon.data.remote.dtos.EpisodesDto
 import com.bacon.data.remote.dtos.RickAndMortyResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface EpisodesApiService {
@@ -11,4 +12,10 @@ interface EpisodesApiService {
     suspend fun fetchEpisodes(
         @Query("page") page: Int,
     ): RickAndMortyResponseDto<EpisodesDto>
+
+    @GET("/api/episode/{id}")
+    suspend fun fetchEpisode(
+        @Path("id") id: Int,
+    ): EpisodesDto
+
 }
