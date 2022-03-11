f22aac76641307aa335dec2a6a55bb0bf82fcbc5
Admin <baconnkl@gmail.com>
2022-03-11 15:43:51 +0300

    added IBaseDiffModel

--- app/src/main/java/com/example/rickandmortyarchitecture/base/BaseDiffUtilCallback.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/base/BaseDiffUtilCallback.kt
@@ -3,15 +3,15 @@ package com.example.rickandmortyarchitecture.base
 import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 
-class BaseDiffUtilCallback<T> : DiffUtil.ItemCallback<T>() {
+class BaseDiffUtilCallback<T : IBaseDiffModel> : DiffUtil.ItemCallback<T>() {
     override fun areItemsTheSame(
         oldItem: T,
-        newItem: T
-    ) = oldItem === newItem
+        newItem: T,
+    ) = oldItem.id == newItem.id
 
     @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(
         oldItem: T,
-        newItem: T
-    ) = oldItem == newItem
+        newItem: T,
+    ) = oldItem.id == newItem.id
 }
