1db9daede25d5d5f58ed64d07be81c1078e1adf8
Admin <baconnkl@gmail.com>
2022-03-11 15:48:12 +0300

    LocationsUI

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/models/LocationsUI.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/models/LocationsUI.kt
@@ -1,14 +1,15 @@
 package com.example.rickandmortyarchitecture.presentation.models
 
 import com.bacon.domain.models.LocationsModel
+import com.example.rickandmortyarchitecture.base.IBaseDiffModel
 
 data class LocationsUI(
-    var id: Int? = null,
+    override val id: Int,
     var name: String? = null,
     var type: String? = null,
     var dimension: String? = null,
     var created: String? = null,
-)
+) : IBaseDiffModel
 
 fun LocationsModel.toUI() = LocationsUI(
     id, name, type, dimension, created
