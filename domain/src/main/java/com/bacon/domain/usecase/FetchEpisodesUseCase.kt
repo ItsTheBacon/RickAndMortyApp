ae948b66f2bd96351c9471f45c372edbc6219e63
Admin <baconnkl@gmail.com>
2022-03-11 16:53:13 +0300

    FetchEpisodesUseCase

--- domain/src/main/java/com/bacon/domain/usecase/FetchEpisodesUseCase.kt
+++ domain/src/main/java/com/bacon/domain/usecase/FetchEpisodesUseCase.kt
@@ -3,6 +3,6 @@ package com.bacon.domain.usecase
 import com.bacon.domain.repository.EpisodesRepository
 import javax.inject.Inject
 
-class EpisodesUseCase @Inject constructor(private val repository: EpisodesRepository) {
+class FetchEpisodesUseCase @Inject constructor(private val repository: EpisodesRepository) {
     operator fun invoke(page: Int) = repository.fetchEpisodes(page)
 }
