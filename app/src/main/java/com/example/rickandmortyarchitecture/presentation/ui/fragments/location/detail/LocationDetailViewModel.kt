928e8088c6495a6895bf7c879682ebc750556244
Admin <baconnkl@gmail.com>
2022-03-11 15:43:03 +0300

    LocationDetailViewModel

---
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/location/detail/LocationDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.rickandmortyarchitecture.presentation.ui.fragments.location.detail
+
+import com.example.rickandmortyarchitecture.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class LocationDetailViewModel @Inject constructor() : BaseViewModel() {
+}
