6af55a96e5a633bfeed71d48c9096262d53608b1
Admin <baconnkl@gmail.com>
2022-03-11 15:47:11 +0300

    minor

--- domain/src/main/java/com/bacon/domain/models/EpisodesModel.kt
+++ domain/src/main/java/com/bacon/domain/models/EpisodesModel.kt
@@ -2,7 +2,7 @@ package com.bacon.domain.models
 
 data class EpisodesModel(
 
-    var id: Int? = null,
+    val id: Int,
     var name: String? = null,
     var air_date: String? = null,
     var episode: String? = null,
