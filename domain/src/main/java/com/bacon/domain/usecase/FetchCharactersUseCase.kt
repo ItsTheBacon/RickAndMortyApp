2f60d27b36d645d1be0b0f6abd3ebfade8f57c31
Admin <baconnkl@gmail.com>
2022-03-11 16:53:03 +0300

    FetchCharactersUseCase

--- domain/src/main/java/com/bacon/domain/usecase/FetchCharactersUseCase.kt
+++ domain/src/main/java/com/bacon/domain/usecase/FetchCharactersUseCase.kt
@@ -3,6 +3,6 @@ package com.bacon.domain.usecase
 import com.bacon.domain.repository.CharacterRepository
 import javax.inject.Inject
 
-class CharacterUseCase @Inject constructor(private val repository: CharacterRepository) {
+class FetchCharactersUseCase @Inject constructor(private val repository: CharacterRepository) {
     operator fun invoke(page: Int) = repository.fetchCharacters(page)
 }
