71cfa3f431461fd5969031ccb9a8398064f2bac2
Admin <baconnkl@gmail.com>
2022-03-11 16:53:20 +0300

    FetchLocationsUseCase

--- domain/src/main/java/com/bacon/domain/usecase/FetchLocationsUseCase.kt
+++ domain/src/main/java/com/bacon/domain/usecase/FetchLocationsUseCase.kt
@@ -3,6 +3,6 @@ package com.bacon.domain.usecase
 import com.bacon.domain.repository.LocationRepository
 import javax.inject.Inject
 
-class LocationUseCase @Inject constructor(private val repository: LocationRepository) {
+class FetchLocationsUseCase @Inject constructor(private val repository: LocationRepository) {
     operator fun invoke(page: Int) = repository.fetchLocation(page)
 }
