b1fc4b9e6eaf1a56086c814cc37a2ae57acc19bf
Admin <baconnkl@gmail.com>
2022-03-14 17:31:51 +0300

    delete repository intereface

--- domain/src/main/java/com/bacon/domain/repository/CharactersDetailRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bacon.domain.repository
-
-import com.bacon.common.resouce.Resource
-import com.bacon.domain.models.CharactersModel
-import kotlinx.coroutines.flow.Flow
-
-interface CharactersDetailRepository {
-     fun fetchCharacterById(id: Int): Flow<Resource<CharactersModel>>
-}
