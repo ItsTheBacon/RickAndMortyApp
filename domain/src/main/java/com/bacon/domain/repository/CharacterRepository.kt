4569e502ca0b7b39333b39d89cf799af19bb5988
Admin <baconnkl@gmail.com>
2022-03-14 17:30:38 +0300

    old characters Repositories

--- domain/src/main/java/com/bacon/domain/repository/CharacterRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bacon.domain.repository
-
-import com.bacon.common.resouce.Resource
-import com.bacon.domain.models.CharactersModel
-import kotlinx.coroutines.flow.Flow
-
-interface CharacterRepository {
-    fun fetchCharacters(page: Int): Flow<Resource<List<CharactersModel>>>
-}
