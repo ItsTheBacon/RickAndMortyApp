3ae83df17df5fbe4488540e7a8334aa61a4b7748
Admin <baconnkl@gmail.com>
2022-03-14 17:32:32 +0300

    delete useCases

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/detail/CharactersDetailViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/detail/CharactersDetailViewModel.kt
@@ -1,9 +1,8 @@
 package com.example.rickandmortyarchitecture.presentation.ui.fragments.characters.detail
 
-import com.bacon.domain.usecase.FetchCharacterDetailUseCase
+import com.bacon.data.remote.dtos.CharactersDto
+import com.bacon.data.remote.repository.CharactersDetailRepository
 import com.example.rickandmortyarchitecture.base.BaseViewModel
-import com.example.rickandmortyarchitecture.presentation.models.CharactersUI
-import com.example.rickandmortyarchitecture.presentation.models.toUI
 import com.example.rickandmortyarchitecture.presentation.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,14 +11,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CharactersDetailViewModel @Inject constructor(
-    private val useCase: FetchCharacterDetailUseCase,
+    private val repositoryImpl: CharactersDetailRepository,
 ) : BaseViewModel() {
-    private val _charactersDetailState = MutableStateFlow<UIState<CharactersUI>>(UIState.Loading())
-    val charactersDetailState: StateFlow<UIState<CharactersUI>> = _charactersDetailState
+    private val _charactersDetailState = MutableStateFlow<UIState<CharactersDto>>(UIState.Loading())
+    val charactersDetailState: StateFlow<UIState<CharactersDto>> = _charactersDetailState
 
     fun fetchCharactersById(id: Int) {
-        useCase(id).collectRequest(_charactersDetailState) {
-            it.toUI()
+        _charactersDetailState.subscribeTo {
+            repositoryImpl.fetchCharacterById(id)
         }
     }
 }
