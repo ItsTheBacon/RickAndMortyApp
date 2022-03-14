84eb3e77ad8d0e430bac96a7a7b361364931a500
Admin <baconnkl@gmail.com>
2022-03-14 17:33:25 +0300

    change request

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -1,38 +1,21 @@
 package com.example.rickandmortyarchitecture.presentation.ui.fragments.characters
 
-import com.bacon.domain.usecase.FetchCharactersUseCase
-import com.bacon.domain.usecase.FetchEpisodesDetailUseCase
-import com.example.rickandmortyarchitecture.base.BaseFetch
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.bacon.data.remote.repository.CharacterRepository
+import com.bacon.data.remote.repository.EpisodeDetailRepository
 import com.example.rickandmortyarchitecture.base.BaseViewModel
-import com.example.rickandmortyarchitecture.presentation.models.CharactersUI
-import com.example.rickandmortyarchitecture.presentation.models.toUI
-import com.example.rickandmortyarchitecture.presentation.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    private val fetchCharactersUseCase: FetchCharactersUseCase,
-    private val fetchEpisodesDetailUseCase: FetchEpisodesDetailUseCase,
-) : BaseViewModel(), BaseFetch {
-    private val _charactersState = MutableStateFlow<UIState<List<CharactersUI>>>(UIState.Loading())
-    val charactersState: StateFlow<UIState<List<CharactersUI>>> = _charactersState
-    override var page: Int = 1
+    private val repository: CharacterRepository,
+    private val repositoryEpisode: EpisodeDetailRepository,
+) : BaseViewModel() {
+    fun fetchCharacters() = repository.fetchCharacters().cachedIn(viewModelScope)
 
-    init {
-        fetchRick(1)
-    }
-
-    override fun fetchRick(page: Int) {
-        fetchCharactersUseCase(page).collectRequest(_charactersState) { it ->
-            it.map {
-                it.toUI()
-            }
-        }
-    }
-
-    fun fetchEpisode(id: Int) = fetchEpisodesDetailUseCase(id)
+    fun fetchEpisode(id: Int) = repositoryEpisode.fetchEpisode(id).asLiveData()
 
 }
