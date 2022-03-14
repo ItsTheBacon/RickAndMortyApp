87f84fd9870156df0ac9f73aaa895f478813c576
Admin <baconnkl@gmail.com>
2022-03-14 17:41:11 +0300

    added paging

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/location/LocationViewModel.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/ui/fragments/location/LocationViewModel.kt
@@ -1,33 +1,16 @@
 package com.example.rickandmortyarchitecture.presentation.ui.fragments.location
 
-import com.bacon.domain.usecase.FetchLocationsUseCase
-import com.example.rickandmortyarchitecture.base.BaseFetch
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.bacon.data.remote.repository.LocationRepository
 import com.example.rickandmortyarchitecture.base.BaseViewModel
-import com.example.rickandmortyarchitecture.presentation.models.LocationsUI
-import com.example.rickandmortyarchitecture.presentation.models.toUI
-import com.example.rickandmortyarchitecture.presentation.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationViewModel @Inject constructor(
-    private val useCase: FetchLocationsUseCase,
-) : BaseViewModel(), BaseFetch {
-    private val _locationState = MutableStateFlow<UIState<List<LocationsUI>>>(UIState.Loading())
-    val locationState: StateFlow<UIState<List<LocationsUI>>> = _locationState
-    override var page: Int = 1
-
-    init {
-        fetchRick(1)
-    }
-
-    override fun fetchRick(page: Int) {
-        useCase(page).collectRequest(_locationState) { it ->
-            it.map {
-                it.toUI()
-            }
-        }
-    }
+    private val repository: LocationRepository,
+) : BaseViewModel() {
+    fun fetchLocations() =
+        repository.fetchLocation().cachedIn(viewModelScope)
 }
