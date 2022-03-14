7fa33b63045449f41172f9a6552f63cb114bab85
Admin <baconnkl@gmail.com>
2022-03-14 17:34:50 +0300

    delete EpisodesDetailRepository  interface

--- domain/src/main/java/com/bacon/domain/repository/EpisodesDetailRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bacon.domain.repository
-
-import com.bacon.common.resouce.Resource
-import com.bacon.domain.models.EpisodesModel
-import kotlinx.coroutines.flow.Flow
-
-interface EpisodesDetailRepository {
-    fun fetchEpisode(id: Int): Flow<Resource<EpisodesModel>>
-}
