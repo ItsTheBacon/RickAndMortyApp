490547bd327f3ddeed1bb974cf2b89e3a10e7120
Admin <baconnkl@gmail.com>
2022-07-01 12:26:03 +0600

    change Resource to Either

--- domain/src/main/java/com/bacon/domain/repository/CharactersDetailRepository.kt
+++ domain/src/main/java/com/bacon/domain/repository/CharactersDetailRepository.kt
@@ -1,9 +1,9 @@
 package com.bacon.domain.repository
 
-import com.bacon.common.resouce.Resource
+import com.bacon.common.either.Either
 import com.bacon.domain.models.CharactersModel
 import kotlinx.coroutines.flow.Flow
 
 interface CharactersDetailRepository {
-     fun fetchCharacterById(id: Int): Flow<Resource<CharactersModel>>
+     fun fetchCharacterById(id: Int): Flow<Either<String, CharactersModel>>
 }
