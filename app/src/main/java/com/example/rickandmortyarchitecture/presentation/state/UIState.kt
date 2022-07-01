713c850e01c77bfbf28d7715c50b3f75fcc14be5
Admin <baconnkl@gmail.com>
2022-07-01 12:29:13 +0600

    added State Idle for before collect

--- app/src/main/java/com/example/rickandmortyarchitecture/presentation/state/UIState.kt
+++ app/src/main/java/com/example/rickandmortyarchitecture/presentation/state/UIState.kt
@@ -2,6 +2,7 @@ package com.example.rickandmortyarchitecture.presentation.state
 
 
 sealed class UIState<T> {
+    class Idle<T> : UIState<T>()
     class Loading<T> : UIState<T>()
     class Error<T>(val error: String) : UIState<T>()
     class Success<T>(val data: T) : UIState<T>()
