71c4294de64fc43465e9f39c9bacb2b0291fd83c
Admin <baconnkl@gmail.com>
2022-07-01 12:24:03 +0600

    upGrade fun doRequest Change Sealed Class Resource to Either

--- data/src/main/java/com/bacon/data/repository/base/BaseRepository.kt
+++ data/src/main/java/com/bacon/data/repository/base/BaseRepository.kt
@@ -1,21 +1,17 @@
 package com.bacon.data.repository.base
 
-import com.bacon.common.resouce.Resource
+import com.bacon.common.either.Either
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 abstract class BaseRepository {
 
-    protected fun <T> doRequest(request: suspend () -> T) = flow {
-        emit(Resource.Loading())
-        try {
-            emit(Resource.Success(data = request()))
-        } catch (ioException: Exception) {
-            emit(
-                Resource.Error(
-                    data = null,
-                    message = ioException.localizedMessage ?: "Error Occurred!"
-                )
-            )
-        }
+    protected fun <T> doRequest(request: suspend () -> T) = flow<Either<String, T>> {
+        emit(Either.Right(value = request()))
+    }.flowOn(Dispatchers.IO).catch { exception ->
+        emit(Either.Left(value = exception.localizedMessage ?: "Error Occurred!"))
     }
+
 }
