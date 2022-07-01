60cbde6ee52c180c15b23589f7a2842085eb1af4
Admin <baconnkl@gmail.com>
2022-07-01 12:27:36 +0600

    change Resource to Either

--- domain/src/main/java/com/bacon/domain/repository/EpisodesRepository.kt
+++ domain/src/main/java/com/bacon/domain/repository/EpisodesRepository.kt
@@ -1,10 +1,11 @@
 package com.bacon.domain.repository
 
+import com.bacon.common.either.Either
 import com.bacon.common.resouce.Resource
 import com.bacon.domain.models.EpisodesModel
 import kotlinx.coroutines.flow.Flow
 
 interface EpisodesRepository {
-    fun fetchEpisodes(page: Int): Flow<Resource<List<EpisodesModel>>>
+    fun fetchEpisodes(page: Int): Flow<Either<String,List<EpisodesModel>>>
 
 }
