e07097dd874496b99dd57537c975d12241ef829a
Admin <baconnkl@gmail.com>
2022-07-01 12:25:42 +0600

    delete space

--- data/src/main/java/com/bacon/data/repository/CharacterRepositoryImpl.kt
+++ data/src/main/java/com/bacon/data/repository/CharacterRepositoryImpl.kt
@@ -2,19 +2,18 @@ package com.bacon.data.repository
 
 import com.bacon.data.remote.apiservices.CharacterApiService
 import com.bacon.data.remote.dtos.toCharacter
-import com.bacon.data.remote.dtos.toResponse
 import com.bacon.data.repository.base.BaseRepository
 import com.bacon.domain.repository.CharacterRepository
 import javax.inject.Inject
 
 class CharacterRepositoryImpl @Inject constructor(
     private val service: CharacterApiService,
-) : BaseRepository(),
-    CharacterRepository {
+) : BaseRepository(), CharacterRepository {
     override fun fetchCharacters(page: Int) = doRequest {
-        service.fetchCharacters(page).toResponse().results.map {
+        service.fetchCharacters(page).results.map {
             it.toCharacter()
         }
     }
+
 
 }
