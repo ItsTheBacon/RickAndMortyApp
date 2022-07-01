b055cca010ab5abe6f0d658f70f24406a5ceae69
Admin <baconnkl@gmail.com>
2022-07-01 12:28:46 +0600

    change Resource to Either

--- domain/src/main/java/com/bacon/domain/repository/LocationRepository.kt
+++ domain/src/main/java/com/bacon/domain/repository/LocationRepository.kt
@@ -1,10 +1,10 @@
 package com.bacon.domain.repository
 
-import com.bacon.common.resouce.Resource
+import com.bacon.common.either.Either
 import com.bacon.domain.models.LocationsModel
 import kotlinx.coroutines.flow.Flow
 
 interface LocationRepository {
-    fun fetchLocation(page: Int): Flow<Resource<List<LocationsModel>>>
+    fun fetchLocation(page: Int): Flow<Either<String, List<LocationsModel>>>
 
 }
