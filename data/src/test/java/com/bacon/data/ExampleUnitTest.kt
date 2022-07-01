ea45b8ca4e990f4fad3ecde6820c0fa3a4bd28a2
Admin <baconnkl@gmail.com>
2022-07-01 14:20:58 +0600

    delete imports

--- data/src/test/java/com/bacon/data/ExampleUnitTest.kt
+++ data/src/test/java/com/bacon/data/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.bacon.data
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
